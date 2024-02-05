46f8dc38ab44a91939f8cb521acd1f562f7f7142
AndreFerreira5 <andre.s.ferreira.vip@gmail.com>
2024-02-05 18:25:33 +0000

    initial sensor code

--- src/sensor.c
+++ src/sensor.c
@@ -1,1 +1,16 @@
 #include "sensor.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+
+    if(argc != 6){
+        if(argc < 6)
+            fprintf(stdout, "Arguments missing!\nUsage: sensor *sensorID* *interval (seconds)* *key* *min value* *max value*\n");
+        else
+            fprintf(stdout, "Too many arguments!\n");
+        exit(1);
+    }
+
+
+}
