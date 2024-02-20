0a9b7f39bc53f0416c3e88e45db1606426331330
AndreFerreira5 <andre.s.ferreira.vip@gmail.com>
2024-02-20 22:02:42 +0000

    sensors & alerts shmem

--- src/home_iot.h
+++ src/home_iot.h
@@ -9,4 +9,21 @@ typedef struct{
 }shared_ring_buffer;
 extern shared_ring_buffer *ring_buffer_shmem;
 
+typedef struct{
+    char id[32];
+    char key[32];
+    long value;
+}sensor;
+
+typedef struct{
+    char id[32];
+    long min;
+    long max;
+}alert;
+
+typedef struct{
+    sensor* sensors;
+    alert* alerts;
+}sensors_alerts;
+
 #endif //IOT_SYSTEM_SIM_HOME_IOT_H
