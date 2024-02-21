fe91cd3a5f593477ba0bd694f7c0a3de92fecbb0
AndreFerreira5 <andre.s.ferreira.vip@gmail.com>
2024-02-21 00:10:25 +0000

    process task based on its type

--- src/worker.c
+++ src/worker.c
@@ -3,6 +3,22 @@
 #include <stdio.h>
 #include <unistd.h>
 
+
+void process_sensor_data(node* task){
+    if(strcmp(task->sensorData->key, "DEAD")==0){
+        // remove sensor from hash table
+    }
+    // if sensor id doesnt exist on hash table, add it
+        // if the hash table has reached the max sensor size, remove the sensor that hasnt responded in the longest time (possibly the DEAD signal has been dropped and didnt reach the workers)
+    // if it exists, get the array position of it and update its values
+}
+
+
+void process_user_console_data(node* task){
+
+}
+
+
 _Noreturn void init_worker(sensors_alerts* sensors_alerts_shmem, maxHeap* taskHeap){
     long pid = (long)getpid();
 
@@ -23,7 +39,16 @@ _Noreturn void init_worker(sensors_alerts* sensors_alerts_shmem, maxHeap* taskHe
         fprintf(stdout, "[WORKER %ld] GOT TASK FROM HEAP\n", pid);
         #endif
 
-
+        switch(task.type){
+            case SENSOR_DATA:
+                process_sensor_data(&task);
+                break;
+            case USER_CONSOLE_DATA:
+                // process_user_console_data(&task);
+                break;
+            default:
+                break;
+        }
 
 
         // lock shmem mutex
